593c677eddd48927579fe7e2306356900b39e68a
charlespeters <charlespeters42@gmail.com>
2014-07-29 14:11:27 -0400

    finalizing a project structure.

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,59 @@
-var gulp = require('gulp'),
-    uglify = require('gulp-uglify'),
-    sass = require('gulp-ruby-sass'),
-    plumber = require('gulp-plumber'),
-    serve = require('gulp-serve'),
-    prefix = require('gulp-autoprefixer');
-
-
+var gulp         = require('gulp'),
+    uglify       = require('gulp-uglify'),
+    sass         = require('gulp-sass'),
+    plumber      = require('gulp-plumber'),
+    autoprefixer = require('gulp-autoprefixer'),
+    connect      = require('gulp-connect'),
+    concat       = require('gulp-concat'),
+    imagemin     = require('gulp-imagemin');
 
 gulp.task('scripts', function(){
-    gulp.src('javascript/*.js')
+    gulp.src('src/js/*.js')
         .pipe(plumber())
         .pipe(uglify())
-        .pipe(gulp.dest('build/js/'));
+        .pipe(concat('justafewicons.min.js'))
+        .pipe(gulp.dest('build/js/'))
+        .pipe(connect.reload());
 });
 
 gulp.task('styles', function(){
-    gulp.src('scss/**/*.scss')
+    gulp.src('src/scss/**/*.scss')
         .pipe(plumber())
         .pipe(sass({style: 'compressed', sourcemap: true}))
-        .pipe(gulp.dest('build/css/'));
+        .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+        .pipe(gulp.dest('build/css/'))
+        .pipe(connect.reload());
+});
+
+gulp.task('html', function () {
+  gulp.src('src/index.html')
+    .pipe(gulp.dest('build/'))
+    .pipe(connect.reload());
+});
+
+gulp.task('templates', function() {
+  gulp.src('./lib/*.html')
+    .pipe(swig())
+    .pipe(gulp.dest('./dist/'));
 });
 
 gulp.task('watch', function(){
-    gulp.watch('javascript/*.js', ['scripts']);
-    gulp.watch('scss/**/*.scss', ['styles']);
+    gulp.watch('src/js/*.js', ['scripts']);
+    gulp.watch('src/scss/**/*.scss', ['styles']);
+    gulp.watch(['src/index.html'], ['html']);
 });
 
-gulp.task('default', ['scripts', 'styles', 'watch']);
+gulp.task('connect', function() {
+    connect.server({ root: 'build', livereload: true });
+});
+
+gulp.task('images', function () {
+    return gulp.src('src/img/*')
+        .pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}]
+        }))
+        .pipe(gulp.dest('build/img/'));
+});
+
+gulp.task('default', ['html','scripts', 'styles', 'images', 'watch', 'connect']);
