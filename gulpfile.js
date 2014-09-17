975fe0ed3a88b18939d851563b383afd7253d923
charlespeters <charlespeters42@gmail.com>
2014-09-16 20:17:08 -0400

    making smarter changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,55 +1,19 @@
 var gulp         = require('gulp'),
-    uglify       = require('gulp-uglify'),
-    sass         = require('gulp-sass'),
     plumber      = require('gulp-plumber'),
-    autoprefixer = require('gulp-autoprefixer'),
-    connect      = require('gulp-connect'),
-    concat       = require('gulp-concat'),
-    imagemin     = require('gulp-imagemin');
-
-gulp.task('scripts', function(){
-    gulp.src('src/js/*.js')
-        .pipe(plumber())
-        .pipe(uglify())
-        .pipe(concat('justafewicons.min.js'))
-        .pipe(gulp.dest('build/js/'))
-        .pipe(connect.reload());
-});
-
-gulp.task('styles', function(){
-    gulp.src('src/scss/**/*.scss')
-        .pipe(plumber())
-        .pipe(sass({style: 'compressed', sourcemap: true}))
-        .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-        .pipe(gulp.dest('build/css/'))
-        .pipe(connect.reload());
-});
-
-gulp.task('html', function () {
-  gulp.src('src/index.html')
-    .pipe(gulp.dest('build/'))
-    .pipe(connect.reload());
-});
-
+    svgstore     = require('gulp-svgstore'),
+    svgmin       = require('gulp-svgmin');
 
 
 gulp.task('watch', function(){
-    gulp.watch('src/js/*.js', ['scripts']);
-    gulp.watch('src/scss/**/*.scss', ['styles']);
-    gulp.watch(['src/index.html'], ['html']);
+    gulp.watch('svg/*svg', ['svg']);
 });
 
-gulp.task('connect', function() {
-    connect.server({ root: 'build', livereload: true });
+
+gulp.task('svg', function () {
+  return gulp.src('icons/*.svg')
+             .pipe(svgstore({ fileName: 'defs-block.svg', prefix: 'icon-'}))
+             .pipe(svgmin())
+             .pipe(gulp.dest('./'));
 });
 
-gulp.task('images', function () {
-    return gulp.src('src/img/*')
-        .pipe(imagemin({
-            progressive: true,
-            svgoPlugins: [{removeViewBox: false}]
-        }))
-        .pipe(gulp.dest('build/img/'));
-});
-
-gulp.task('default', ['html', 'scripts', 'styles', 'images', 'watch', 'connect']);
+gulp.task('default', ['svg', 'watch']);
