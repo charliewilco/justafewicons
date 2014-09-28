3b5f1a5f47998959433d022e8d175e2ad00d9cb8
charlespeters <charlespeters42@gmail.com>
2014-09-28 17:54:58 -0400

    updating gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,46 +3,48 @@ var gulp         = require('gulp'),
     sass         = require('gulp-sass'),
     plumber      = require('gulp-plumber'),
     autoprefixer = require('gulp-autoprefixer'),
-    connect      = require('gulp-connect'),
+    webserver    = require('gulp-webserver'),
     concat       = require('gulp-concat'),
+    livereload   = require('gulp-livereload'),
     imagemin     = require('gulp-imagemin');
 
-gulp.task('scripts', function(){
-    gulp.src('js/*.js')
-        .pipe(plumber())
-        .pipe(uglify())
-        .pipe(concat('justafewicons.min.js'))
-        .pipe(gulp.dest('build/js/'))
-        .pipe(connect.reload());
-});
+// gulp.task('scripts', function(){
+//     gulp.src('js/*.js')
+//         .pipe(plumber())
+//         .pipe(uglify())
+//         .pipe(concat('justafewicons.min.js'))
+//         .pipe(gulp.dest('build/js/'))
+//         .pipe(connect.reload());
+// });
 
 gulp.task('styles', function(){
     gulp.src('scss/**/*.scss')
         .pipe(plumber())
         .pipe(sass({style: 'compressed', sourcemap: true}))
         .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-        .pipe(gulp.dest('stylesheets/'))
-        .pipe(connect.reload());
+        .pipe(gulp.dest('stylesheets/'));
 });
 
 gulp.task('html', function () {
   gulp.src('index.html')
-    .pipe(gulp.dest('./'))
-    .pipe(connect.reload());
+    .pipe(gulp.dest('./'));
 });
 
 
 
 gulp.task('watch', function(){
-    gulp.watch('js/*.js', ['scripts']);
     gulp.watch('scss/**/*.scss', ['styles']);
     gulp.watch(['index.html'], ['html']);
 });
 
-gulp.task('connect', function() {
-    connect.server({ root: './', livereload: true });
+
+gulp.task('webserver', function() {
+  gulp.src('./')
+    .pipe(webserver({
+      livereload: true,
+      open: true
+    }));
 });
 
 
-
-gulp.task('default', ['html', 'scripts', 'styles', 'watch', 'connect']);
+gulp.task('default', ['html', 'styles', 'watch', 'webserver']);
